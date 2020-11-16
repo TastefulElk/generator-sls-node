410c019b883031fbf0ea395210e3c2d0f331aa56
Sebastian Pettersson <sebastian.pettersson@live.com>
2020-11-16 23:40:31 +0100

    ci: add .releaserc

---
+++ .releaserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  release: {
+    branch: 'master',
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/changelog',
+      '@semantic-release/github',
+      '@semantic-release/npm',
+      '@semantic-release/release-notes-generator',
+    ],
+  },
+};
