9bbf7173205f91320ea585fe25b974ce16e5e43e
Sebastian Pettersson <sebastian.pettersson@live.com>
2020-11-28 23:49:52 +0100

    fix: lint errors

--- generators/app/utility.js
+++ generators/app/utility.js
@@ -1,8 +1,7 @@
-const kebabCase = (str) =>
-  str
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
-    .replace(/\s+/g, '-')
-    .toLowerCase();
+const kebabCase = (str) => str
+  .replace(/([a-z])([A-Z])/g, '$1-$2')
+  .replace(/\s+/g, '-')
+  .toLowerCase();
 
 module.exports = {
   kebabCase,
