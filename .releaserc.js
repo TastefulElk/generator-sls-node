5bac49c8b6c9f0099850e2a7d389de1c343d670e
Sebastian Pettersson <sebastian.pettersson@live.com>
2020-11-29 01:24:02 +0100

    ci: change order of semantic-release plugins

--- .releaserc.js
+++ .releaserc.js
@@ -3,10 +3,10 @@ module.exports = {
     branch: 'master',
     plugins: [
       '@semantic-release/commit-analyzer',
-      '@semantic-release/changelog',
-      '@semantic-release/github',
-      '@semantic-release/npm',
       '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/github',
       '@semantic-release/git'
     ],
   },
